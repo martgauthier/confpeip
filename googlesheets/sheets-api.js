f0bdf26652a86a9fdb33444dca02b7188567413c
Gauthier <gauthier.martin@etu.univ-nantes.fr>
2023-01-25 08:31:45 +0100

    réglage bug nombre de lignes

--- googlesheets/sheets-api.js
+++ googlesheets/sheets-api.js
@@ -34,7 +34,7 @@ module.exports = class SheetsAPI {
 
     async addDataForDay(dataSans, dataAvec, date=((new Date()).toLocaleDateString("fr-FR") + "-" + (new Date()).getHours().toString().padStart(2, "0"))) { //date au format 01/01/1970 pour le 1er janvier 1970
         let rowIndex = -1
-        for (let i = 1; i <=2300; i++) {//100 pour prendre un grand nombre de jours
+        for (let i = 1; i <=2200; i++) {//100 pour prendre un grand nombre de jours
             if(this.sans.rows[i].Date === date) rowIndex=i;
             //console.log(this.sans.rows[i])
         }
